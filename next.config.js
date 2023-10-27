77ff87e02a1375f114075517477b64f4fda3bb2e
BanDoid <dbbhuh5@gmail.com>
2023-10-27 21:57:48 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // forceSwcTransforms: false,
+    // swcMinify: false,
+    // swcPlugins: [
+    //   ["@next/swc-android-arm64", {}],
+    //   ["@swc/core-android-arm64", {}],
+    // ],
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Alert } from 'flowbite-react'
+
+export default function Home() {
+  return (
+    <main className="p-24 flex flex-col items-center justify-center">
+      <Alert>test</Alert>
+    </main>
+  )
+}
